7596ba0dcb99d308c551866f6f2be870beee4b80
DmitriyBadeev <dmitriibadeev@gmail.com>
2021-01-10 15:35:20 +0500

    remove unused import

--- src/pages/Market.tsx
+++ src/pages/Market.tsx
@@ -1,7 +1,6 @@
 import React from "react"
 import { H3 } from "GeneralStyles"
 import FadePage from "components/fade/FadePage"
-import Loading from "components/loading/Loading"
 
 const Market: React.FC = () => {
     return (
